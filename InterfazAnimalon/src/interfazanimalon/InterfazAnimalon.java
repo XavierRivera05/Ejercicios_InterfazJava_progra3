be26709311d33cea60ea5673363d369a87334d42
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-03-13 20:04:35 -0600

    Ejercicio 1: Animal Batista

---
+++ InterfazAnimalon/src/interfazanimalon/InterfazAnimalon.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfazanimalon;
+
+/**
+ *
+ * @author luisr
+ */
+public class InterfazAnimalon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
