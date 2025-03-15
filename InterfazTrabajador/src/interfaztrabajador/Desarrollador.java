f59097152b16df4b6d284cfb83107fc8c83cf28c
XavierRivera05 <126819995+XavierRivera05@users.noreply.github.com>
2025-03-15 13:50:32 -0600

    Ejercicio 5: InterfazTrabajador
    When you get caught between the Moon and New York City!!!

---
+++ InterfazTrabajador/src/interfaztrabajador/Desarrollador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaztrabajador;
+
+/**
+ *
+ * @author luisr
+ */
+public class Desarrollador implements Trabajador {
+    @Override
+    public void trabajar()
+    {
+        System.out.println("El desarrolador está escribiendo un código bien codigoso");
+    }
+}
---
+++ InterfazTrabajador/src/interfaztrabajador/Diseñador.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaztrabajador;
+
+/**
+ *
+ * @author luisr
+ */
+public class Diseñador implements Trabajador {
+    @Override
+    public void trabajar()
+    {
+        System.out.println("El diseñador está creando diseños diseñosos");
+    }
+    
+}
